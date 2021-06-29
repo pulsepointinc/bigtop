ea6b08c3d399b11a4dd0393e5fda8184e674901f
Alex K aka Aleksei Kozlovskii <aKozlovskii@pulsepoint.com>
2021-06-30 00:54:03 +0300

    Extracted classes to separate files

---
+++ manifests/node_with_roles.pp
@@ -0,0 +1,38 @@
+class bigtop_pp::node_with_roles ($roles = hiera("bigtop::roles")) inherits bigtop_pp::hadoop_cluster_node {
+  require bigtop_pp::common
+
+  define deploy_module($roles) {
+    class { "${name}::deploy":
+    roles => $roles,
+    }
+  }
+
+  $modules = [
+    "alluxio",
+    "flink",
+    "hadoop",
+    "hadoop_hbase",
+    "hadoop_hive",
+    "hadoop_oozie",
+    "hadoop_zookeeper",
+    "hcatalog",
+    "livy",
+    "solr",
+    "spark",
+    "tez",
+    "ycsb",
+    "kerberos",
+    "zeppelin",
+    "kafka",
+    "gpdb",
+    "ambari",
+    "bigtop_utils",
+    "elasticsearch",
+    "logstash",
+    "kibana",
+  ]
+
+  bigtop_pp::node_with_roles::deploy_module { $modules:
+    roles => $roles,
+  }
+}
