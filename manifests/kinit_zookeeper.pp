de7793daef2e321e505e6dc9fc70fcd02f2cc496
Alex K aka Aleksei Kozlovskii <aKozlovskii@pulsepoint.com>
2021-07-07 17:07:24 +0300

    Fixed a typo and removed unsued manifest

--- manifests/kinit_zookeeper.pp
+++
@@ -1,9 +0,0 @@
-class hadoop::kinit_zookeeper {
-  include hadoop::common_hdfs
-
-  exec { "Zookeeper kinit":
-    command => "/usr/bin/kinit -kt /etc/zookeeper.keytab zookeeper/$fqdn && /usr/bin/kinit -R",
-    user    => "zookeeper",
-    require => Kerberos::Host_keytab["zookeeper"],
-  }
-}
