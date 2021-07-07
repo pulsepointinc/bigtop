4edb82c31cfdf26d13fc86c1cf45f639fd4bfd60
Alex K aka Aleksei Kozlovskii <aKozlovskii@pulsepoint.com>
2021-07-07 22:56:28 +0300

    Increased timeout for "init hdfs"

--- manifests/init_hdfs.pp
+++ manifests/init_hdfs.pp
@@ -2,6 +2,7 @@ class hadoop::init_hdfs {
   exec { "init hdfs":
     path    => ['/bin','/sbin','/usr/bin','/usr/sbin'],
     command => 'bash -x /usr/lib/hadoop/libexec/init-hdfs.sh',
-    require => Package['hadoop-hdfs']
+    require => Package['hadoop-hdfs'],
+    timeout => 600
   }
 }
